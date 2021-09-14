32d01bd1f40be2a2011383d61535c2923dae463e
hj-Seong <hjseong1222@gmail.com>
2021-09-14 17:22:36 +0900

    Arrange commit

--- movie_api.py
+++ movie_api.py
@@ -17,7 +17,16 @@ def callMoveApi():
 def convert_model(movies):
     list=[]
     for movie in movies:
+        if len(movie["summary"]) >150:
+            movie["summary"] = movie["summary"][:150]+"..."
+
         movie_model = MovieModel(movie["rating"],movie["summary"],
                         movie["small_cover_image"],movie["title"]) 
+        
         list.append(movie_model)
+        
+    print(type(movies[0]["rating"]))
+    print(type(movies[0]["summary"]))
+    print(type(movies[0]["small_cover_image"]))
+    print(type(movies[0]["title"]))
     return list
